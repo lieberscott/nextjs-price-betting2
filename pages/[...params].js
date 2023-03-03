d2639aa037c1bf6d20c90503965fcf2e20b71384
Scott Lieber <scott@Scotts-MacBook-Air.local>
2023-03-02 21:40:47 -0600

    New commit

--- pages/[...params].js
+++ pages/[...params].js
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router"
 
-export default function MarketsPage() {
+function MarketsPage() {
   const router = useRouter();
 
   const { params = [] } = router.query
   return <h1>Markets home page</h1>
 }
+
+export default MarketsPage;
